ac9d60adb47cac54868776ecf2f9e8ab5ca94a64
Lawrence Ng <lng1492@gmail.com>
2026-05-10 13:38:35 -0400

    build: update url pointing to https://lan13005.github.io/ instead of repo page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://lan13005.github.io',
-  base: '/personal_page',
   integrations: [tailwind({ applyBaseStyles: false }), sitemap(), icon()],
   output: 'static',
   vite: {
